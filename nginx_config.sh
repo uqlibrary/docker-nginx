f112c5df635ad7a0ff43963eafceebb3f618aa78
Andrew Martlew <andrew@martlew.id.au>
2015-02-05 15:12:27 +1000

    Temp test file

--- nginx_config.sh
+++ nginx_config.sh
@@ -2,3 +2,11 @@
 
 sed -i "s/%fpm-ip%/${PHPFPM_SERVICE_HOST}/" /etc/nginx/nginx.conf
 sed -i "s/%fpm-port%/${PHPFPM_SERVICE_PORT}/" /etc/nginx/nginx.conf
+
+# Temporary test
+mkdir -p /data/www/public
+cat > /data/www/public/test.php << EOF
+<?php
+phpinfo();
+
+EOF
