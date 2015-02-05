3b30a1fba0c83089ca0dc7c9e559f76e4e8aaf37
Andrew Martlew <andrew@martlew.id.au>
2015-02-06 09:18:29 +1000

    - Added test text file - Removed pid from conf - Updated supervisor command

--- nginx_config.sh
+++ nginx_config.sh
@@ -5,8 +5,14 @@ sed -i "s/%fpm-port%/${PHPFPM_SERVICE_PORT}/" /etc/nginx/nginx.conf
 
 # Temporary test
 mkdir -p /data/www/public
+
 cat > /data/www/public/test.php << EOF
 <?php
 phpinfo();
+
+EOF
+
+cat > /data/www/public/test.txt << EOF
+Test
 
 EOF
