839e0ee53f2312e504b6ae65c410feaa2c51e27d
Andrew Martlew <andrew@martlew.id.au>
2015-02-10 09:29:20 +1000

    - Configure the container on first load

--- nginx.sh
+++ nginx.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-mkdir -p /data/www
+# Configure on first load..
+if [ ! -f /tmp/supervisord-nginx.log ] ; then
+  mkdir -p /data/www
 
-# Grab the code from an S3 bucket?
-if [ -n "$AWS_S3_OBJECT" ] ; then
-  echo Copying application archive..
-	aws s3 cp $AWS_S3_OBJECT app.zip
-	echo Extracting to web server directory..
-  unzip app.zip -d /data/www
-  cd /data/www
+  # Grab the code from an S3 bucket?
+  if [ -n "$AWS_S3_OBJECT" ] ; then
+    echo Copying application archive..
+    aws s3 cp $AWS_S3_OBJECT app.zip
+    echo Extracting to web server directory..
+    unzip app.zip -d /data/www
+    cd /data/www
 
-  if [ -f .kubernetes/bootstrap.sh ] ; then
-    source .kubernetes/bootstrap.sh
+    if [ -f .kubernetes/bootstrap.sh ] ; then
+      source .kubernetes/bootstrap.sh
+    fi
   fi
+
+  touch /tmp/supervisord-nginx.log
 fi
 
 exec /usr/sbin/nginx
--- php-fpm.sh
+++ php-fpm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# TODO: pass in service environment variables
+# Configure on first load..
+if [ ! -f /tmp/supervisord-phpfpm.log ] ; then
+  # TODO: pass in service environment variables
+
+  touch /tmp/supervisord-phpfpm.log
+fi
 
 exec /usr/sbin/php-fpm
