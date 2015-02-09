575a97072011e9874f10249472b536747f32727e
Andrew Martlew <andrew@martlew.id.au>
2015-02-09 15:00:20 +1000

    - Some debugging

--- nginx_config.sh
+++ nginx_config.sh
@@ -3,6 +3,8 @@
 mkdir -p /data/www/public
 
 if [ -n "$AWS_S3_OBJECT" ] ; then
+  echo Copying application archive..
 	aws s3 cp $AWS_S3_OBJECT app.zip
+	echo Extracting to web server directory..
   unzip app.zip -d /data/www/public
 fi
