022f6244b0b9279f3402a4e5bf1f7c2ed98cf056
Andrew Martlew <andrew@martlew.id.au>
2015-02-10 13:52:32 +1000

    - Setup config on installation of New Relic

--- newrelic.sh
+++ newrelic.sh
@@ -4,8 +4,8 @@ if [ ! -f /tmp/supervisord-newrelic.log ] ; then
   export NR_INSTALL_SILENT=true
   export NR_INSTALL_KEY=${NEWRELIC_INSTALL_KEY}
   newrelic-install install
+  nrsysmond-config --set license_key=${NEWRELIC_INSTALL_KEY}
   touch /tmp/supervisord-newrelic.log
 fi
 
-nrsysmond-config --set license_key=${NEWRELIC_INSTALL_KEY}
-exec /usr/sbin/nrsysmond -c /etc/newrelic/nrsysmond.cfg -l /dev/stdout -f
+exec /usr/sbin/nrsysmond -l /dev/stdout -f
