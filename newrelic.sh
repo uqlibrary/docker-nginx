6d6dc4c344b3f336cae437215f21cfc33cffc5c4
Andrew Martlew <andrew@martlew.id.au>
2015-02-10 14:42:02 +1000

    - Setup Nginx first

--- newrelic.sh
+++ newrelic.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 if [ ! -f /tmp/supervisord-newrelic.log ] ; then
+  # Wait until Nginx has been configured before launching
+  while [ ! -f /tmp/supervisord-nginx.log ]
+  do
+    sleep 2
+  done
+
+  echo Using install key: $NEWRELIC_INSTALL_KEY
+
   export NR_INSTALL_SILENT=true
-  export NR_INSTALL_KEY=${NEWRELIC_INSTALL_KEY}
+  export NR_INSTALL_KEY=$NEWRELIC_INSTALL_KEY
   newrelic-install install
-  nrsysmond-config --set license_key=${NEWRELIC_INSTALL_KEY}
+  nrsysmond-config --set license_key=$NEWRELIC_INSTALL_KEY
   touch /tmp/supervisord-newrelic.log
 fi
 
--- php-fpm.sh
+++ php-fpm.sh
@@ -2,6 +2,11 @@
 
 # Configure on first load..
 if [ ! -f /tmp/supervisord-phpfpm.log ] ; then
+  # Wait until Nginx has been configured before launching
+  while [ ! -f /tmp/supervisord-nginx.log ]
+  do
+    sleep 2
+  done
   # TODO: pass in service environment variables
 
   touch /tmp/supervisord-phpfpm.log
