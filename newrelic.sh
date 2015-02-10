4a3c479329a5f9b9789b00863c37fd92c120b33d
Andrew Martlew <andrew@martlew.id.au>
2015-02-10 10:54:26 +1000

    - Env vars for installing New Relic

--- newrelic.sh
+++ newrelic.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ ! -f /tmp/supervisord-newrelic.log ] ; then
+  export NR_INSTALL_SILENT=true
+  export NR_INSTALL_KEY=${NEWRELIC_INSTALL_KEY}
   newrelic-install install
-  nrsysmond-config --set license_key=${NEWRELIC_INSTALL_KEY}
   touch /tmp/supervisord-newrelic.log
 fi
 
+nrsysmond-config --set license_key=${NEWRELIC_INSTALL_KEY}
 exec /usr/sbin/nrsysmond -c /etc/newrelic/nrsysmond.cfg -l /dev/stdout -f
